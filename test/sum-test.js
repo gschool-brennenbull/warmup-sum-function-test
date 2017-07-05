6f395f8f3835930654ce6ac3a8a6a2361b046a9a
Brennen Bull <brennenbull@gmail.com>
2017-07-05 13:44:59 -0600

    new test

--- test/sum-test.js
+++ test/sum-test.js
@@ -6,13 +6,31 @@ const {expect,assert} = require('chai');
 describe ('Sum', function() {
 
     it ('Should be a function', function() {
-      expect(sum).to.be.a('function','sum is not a function');
       assert.isFunction(sum, 'sum is not a function');
     });
 
     it('Should return 0 if no arguments are passed in.', function() {
-      expect(sum()).to.equal(0);
       assert.equal(sum(),0);
     });
-    
+
+    it('Should return the sum of all numbers passed as arguments', function() {
+      assert.equal(sum(1,10,2,23), 36);
+      assert.equal(sum(0,-1,2,-4), -3);
+      assert.equal(sum(3.2,4.5,5.3), 13);
+    });
+
+    it('Should return the sum of arrays passed as arguments', function(){
+      assert.equal(sum([1,2,3,4,5]), 15);
+      assert.equal(sum([1,3,6],[3,7]), 20);
+    });
+
+    it('Should return the sum of string integers passed as arguments', function () {
+      assert.equal(sum('1','23','6'),30);
+    });
+
+    it('Should return the sum of mixed inputs if arguments can be converted into numbers, else return undefined', function() {
+      assert.equal(sum([1,4],'5', -5), 5);
+      assert.equal(sum('cat', 'dog'), undefined);
+    })
+
 });
